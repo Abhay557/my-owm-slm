780ec432ec1bba4c64721c624831042381917e4d
Abhay <abhaycormourya@gmail.com>
2025-09-28 18:09:02 +0530

    Update generate.py

--- generate.py
+++ generate.py
@@ -2,23 +2,19 @@ import torch
 from model import SLM, ModelConfig
 from tokenizers import Tokenizer
 
-# --- Configuration ---
 MODEL_PATH = "slm_model.pth" # Path to your saved model
 config = ModelConfig()
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-# --- Load Model and Tokenizer ---
+
 print("Loading model and tokenizer...")
-# 1. Create the model with the same architecture (it has random weights)
 model = SLM(config)
-# 2. Load the saved weights from the file
 model.load_state_dict(torch.load(MODEL_PATH))
 model.to(device)
-model.eval() # Set the model to evaluation mode
+model.eval() 
 
 tokenizer = Tokenizer.from_file("tokenizer.json")
 
-# --- Generation Loop ---
 print("\nModel loaded. You can now enter prompts.")
 while True:
     prompt = input("> ")
